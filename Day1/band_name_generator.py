52bdea07dc23fcc0f28b334f73a78d9aaed65df6
ASaHsieh <kchs.tw@gmail.com>
2023-01-09 11:12:19 +0800

    Add newline for seperating questions and answers
    the commit could be merged with the previous one.

--- Day1/band_name_generator.py
+++ Day1/band_name_generator.py
@@ -1,11 +1,11 @@
 #1. Create a greeting for your program.
 print("Welcome to the Band Name Generator.")
 #2. Ask the user for the city that they grew up in.
-city = input("What's name of the city you grew up in? ")
+city = input("What's name of the city you grew up in?\n")
 #3. Ask the user for the name of a pet.
-ans = input("Do you have a pet?[y|n] ")
+ans = input("Do you have a pet?[y|n]\n")
 if ans == "y":
-    pet = input("What's his or her name? ") 
+    pet = input("What's his or her name?\n") 
 else:
     pet = "no_pet"
 #4. Combine the name of their city and pet and show them their band name.
