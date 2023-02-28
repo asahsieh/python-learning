2a37855af623cd6faa7019181afacf1e546bf0f2
ASaHsieh <kchs.tw@gmail.com>
2023-02-28 22:46:45 +0800

    Define Deck class for different turns of game
    - Refactor methods of the class
    - Add safeguards for the methods
    - Remove code for temporarily testing

--- Python_for_Beginners-Full_Course/blackjack.py
+++ Python_for_Beginners-Full_Course/blackjack.py
@@ -10,79 +10,89 @@ References
 
 import random
 
-cards = [] 
-suits = ["spades", "hearts", "diamonds", "clubs"] 
-ranks = [
-        {"rank": "A", "value": 11},
-        {"rank": "2", "value": 2},
-        {"rank": "3", "value": 3},
-        {"rank": "4", "value": 4},
-        {"rank": "5", "value": 5},
-        {"rank": "6", "value": 6},
-        {"rank": "7", "value": 7},
-        {"rank": "8", "value": 8},
-        {"rank": "9", "value": 9},
-        {"rank": "10", "value": 10},
-        {"rank": "J", "value": 10},
-        {"rank": "Q", "value": 10},
-        {"rank": "K", "value": 10}
-    ]
+# Define classes that will be used in order to separate out different aspects of the game
+## Classes provide a way of bundling data and functionality together
+## We're going to use classes to model three parts of the game 
+### A card, a deck, and a hand
+
+class Deck:
+    # always have to pass in `self` to all of these functions in a class because then it gets itself is referring to the instance of the class 
+    # Anything inside the parentheses remember is called an argument
+    def __init__(self):
+        self.cards = [] 
+        suits = ["spades", "hearts", "diamonds", "clubs"] 
+        ranks = [
+                {"rank": "A", "value": 11},
+                {"rank": "2", "value": 2},
+                {"rank": "3", "value": 3},
+                {"rank": "4", "value": 4},
+                {"rank": "5", "value": 5},
+                {"rank": "6", "value": 6},
+                {"rank": "7", "value": 7},
+                {"rank": "8", "value": 8},
+                {"rank": "9", "value": 9},
+                {"rank": "10", "value": 10},
+                {"rank": "J", "value": 10},
+                {"rank": "Q", "value": 10},
+                {"rank": "K", "value": 10}
+            ]
+    
+        # cards should be suits x ranks, total of 52 items or cards
+        for suit in suits:
+            for rank in ranks:
+               self.cards.append([suit, rank])
+    
+    # To shuffled the cards by the `random` module
+    ## random.shuffle(x): Shuffle the sequence x in place.
+    ##
+    ### To shuffle an immutable sequence and return a new shuffled list, use sample(x, k=len(x)) instead.
+    
+    # Using the `self` keyword, the function can access the attributes and methods of the class  
+    def shuffle(self):
+        # A deck with only one card does not need to be shuffled
+        if len(self.cards) > 1:
+            random.shuffle(self.cards)
+        else:
+            print("Only one card, no need to do shuffle.")
+   
+    ## If you want the deal function to deal more than one card, to refactor the deal function  
+    def deal(self, number):
+        cards_dealt = []
+        # For testing the safeguards on IndexError
+        #self.cards = []
+
+        # use a the range function with a for loop
+        for x in range(number):
+            # You can only remove a card if there are cards to remove.
+            ## So, before the program tries to pop a card off self.cards, is to check if the length of self.cards is greater than zero
+            ### Way 1:
+            # try:
+            #     self.cards.pop()
+            # except IndexError:
+            #     print("Oops!  cards is empty.")
+            #     exit(1)
+            # else:
+            #   cards_dealt.append(self.cards.pop())
+
+            ### Way 2 from the course:
+            if len(self.cards) > 0:
+                cards_dealt.append(self.cards.pop())
+            else:
+                print("Oops!  cards is empty.")
+                exit(1)
+
+        return cards_dealt
+
+deck1 = Deck()
+deck2 = Deck()
+deck2.shuffle()
+
+# not shuffled
+print(deck1.cards)
+
+print(deck2.cards)
+
+# Let's add safeguards to prevent errors every time the `deal` function is called, a card is removed from the cards list.
  
-# cards should be suits x ranks, total of 52 items or cards
-for suit in suits:
-   # # a list with suit and ranks
-   # print([suit, ranks[0]]) 
-
-    # let's print every rank in every suit
-    for rank in ranks:
-       # print(suit + " " + rank)
-       print([suit, rank])
-
-       # let's append those 52 cards to the cards list
-       cards.append([suit, rank])
-
-print()
-# All the cards are inorder in the cards lists 
-#print(cards)
-
-# To shuffled the cards by the `random` module
-## random.shuffle(x): Shuffle the sequence x in place.
-##
-### To shuffle an immutable sequence and return a new shuffled list, use sample(x, k=len(x)) instead.
-
-## by a function 
-def shuffle():
-    random.shuffle(cards)
-
-# Move to after the deal() function 
-# shuffle()
-# print(cards)
-# print()
-
-# Let's remove a single element from the cards, this is similar to dealing a card from a deck by the `pop` method
-
-## By a function also 
-def deal():
-    card = cards.pop()
-    return card
-
-## If you want the deal function to deal more than one card, to refactor the deal function  
-def deal_ref(number):
-    cards_dealt = []
-    # use a the range function with a for loop
-    for n in range(number):
-        cards_dealt.append(cards.pop())
-    return cards_dealt
-
-shuffle()
-
-# Refactor the code to get the value of each rank without using an if statement
-## A new variable called `card` and assign to the card variable a single card that will deal from the deck but we'll make sure that card is not in a list 
-## Change 
-
-# A little tricky that dealing one card but to get the first item, it's going to the only item in the list 
-card = deal_ref(1)[0]
-print(card)
-
-# to print value only
-print(f"value: {card[1]['value']}")
+card = deck2.deal(1)
+print(f"\ndealt card: {card}")
