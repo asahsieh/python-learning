b44d57f7c8cf92f871fd43e837d80694ae4accee
ASaHsieh <kchs.tw@gmail.com>
2023-02-28 12:09:09 +0800

    Initial code for Blackjack Card Game Project
    A simple code to represent a card

---
+++ Python_for_Beginners-Full_Course/blackjack.py
@@ -0,0 +1,14 @@
+"""
+Blackjack Card Game Project
+
+To learn the techniques
+- object-oriented programming in python
+
+References
+- https://www.hopenglish.com/a-deck-of-cards
+"""
+
+suit = "hearts"
+rank = "K"
+value = 10
+print("your card is: " + rank + " of " + suit)
